55d745ceafaafe82e6d7ba7ffab1b6c5d36140b7
vrr223 <43069104+vrr223@users.noreply.github.com>
2018-09-21 10:58:48 -0400

    Add files via upload

---
+++ DeLaBriandaisTrieDic.java
@@ -0,0 +1,36 @@
+
+/**
+ * @author vrr8
+ *
+ */
+public class DeLaBriandaisTrieDic 
+{
+	Node root;
+	
+	
+
+	/**
+	 * Constructor for DLBTrieDictionary
+	 * 
+	 */
+	public DeLaBriandaisTrieDic()
+	{
+		root = new Node();
+	}
+	
+}
+
+class Node
+{
+	Node sibling;
+	Node child;
+	
+	/**
+	 * Constructor for Node
+	 */
+	public Node()
+	{
+		
+	}
+	
+}
